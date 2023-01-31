f6720f0dd762778dddb369c4ebb713b28f6b6547
Karan Kohli <karan.kohli@contentful.com>
2023-01-31 16:48:16 +0100

    add flag for incremental ignore

--- src/main.ts
+++ src/main.ts
@@ -211,7 +211,7 @@ async function run(): Promise<void> {
         change_set_environment.set_enable_incremental();
 
         await new ChangeSetFileWriter(logger).write_change_set_file(change_file, changed_files);
-        actual_build_command += " --incremental $CHANGE_SET_FILE_PATH"
+        actual_build_command += " --ignore-uncapturable-inputs true --incremental $CHANGE_SET_FILE_PATH"
         actual_build_command = new ChangeSetReplacement().replace_build_command(actual_build_command, change_file);
       }
 

b4e80f11ac2d8e00aac2b3594c53ffadc49fc0e9
Karan Kohli <karan.kohli@contentful.com>
2023-04-14 19:13:08 +0200

    revert

--- src/main.ts
+++ src/main.ts
@@ -409,7 +409,7 @@ async function run(): Promise<void> {
           newReviewComments.push(createReviewComment(issue, reviewCommentBody))
         } else {
           logger.info('Issue not reported, adding an issue comment.')
-          githubCreateIssueComment(GITHUB_TOKEN, issueCommentBody)
+          // githubCreateIssueComment(GITHUB_TOKEN, issueCommentBody)
         }
       }
 
